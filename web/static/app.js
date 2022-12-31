d2a0d5cc71234a2b1f828e559d5aec5d0fc449fb
piter <aguilargarza43@gmail.com>
2022-12-31 18:00:07 +0000

    python now write in format json correctly

--- web/static/app.js
+++ web/static/app.js
@@ -10,7 +10,7 @@ function log(){//funcion a la que se llama desde html cuando se hace click en el
 
 function comprobar_usuario(datos, inputUser, inputPass){ //esta funcion se encarga de combrobar el usuario y contraseña
     //ingresado con la información de la base de datos
-    if(datos[1] === inputUser && datos[2] === inputPass){ //compara el usuario y contraseña ingresado con la base de datos
+    if(datos.user === inputUser && datos.password === inputPass){ //compara el usuario y contraseña ingresado con la base de datos
         console.log("Usuario encontrado");
         alert("Acceso concedido")
         window.open('/principal', "_self"); // se redirecciona al usuario a la pagina principla si este ingreso credenciales correctas
@@ -31,8 +31,7 @@ function usuarios_db(nombre, contraseña){ //solicita la información a la base 
     .then(data => {
         data.forEach(date => {
             info = date
-            comprobar_usuario(date, nombre, contraseña) //llama a la funcion para comprobar credenciales con cada dato
-            //obtenido de la base de datos
+            comprobar_usuario(date, nombre, contraseña)
         });
     })
 }
--- web/venta.py
+++ web/venta.py
@@ -5,13 +5,22 @@ def data_stock():
     # conexion con la base de datos
     route = db.connect('db/data.sqlite3')
     try:
-        get_stock = route.cursor()
         #obtener la lista de productos de la base de datos
-        stock = get_stock.execute("SELECT * FROM Products").fetchall()
+        stocks = route.execute("SELECT * FROM Products").fetchall()
+        #info_json = []
+        #for product in stocks:
+        #    stock = {
+        #        "id" : product[0],
+        #        "product" : product[1],
+        #        "stock" : product[2],
+        #        "price" : product[3]
+        #    }
+        #    info_json.append(stock)
+        #print(info_json)
         #selecciona un archivo json donde se guardara la informacion
         document = open('static/json/stock.json', 'w')
         #escribir la información en el archivo json
-        json.dump(stock, document)
+        json.dump(stocks, document, indent=4)
         #termina la escritura del documento
         document.close()
     #se ejecuta en caso de un error relacionado con la base de datos
@@ -21,4 +30,4 @@ def data_stock():
     finally:
         print("Programa finalizado")
 
-#data_stock()
+data_stock()
